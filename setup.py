20102d0409c7cdc3324731bdb27c6842a543ff25
jackoaw <worldchuba@gmail.com>
2019-08-15 17:17:47 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+import codecs
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='tdameritradeapi',
+    version='1.5',
+    description="A tool used to interact with TD Ameritrade's API",
+    long_description=description,
+    url='https://github.com/JECSand/yahoofinancials',
+    download_url='https://github.com/stoxrisk/tdameritradeapi',
+    author='Andrew Chuba',
+    author_email='andrew.chuba@live.com',
+    license='MIT',
+    keywords=['finance data', 'stocks', 'derivatives', 'cryptocurrencies', 'currencies', 'forex', 'td ameritrade'],
+    packages=['yahoofinancials'],
+    install_requires=[
+        "requests"
+    ],
+    classifiers=[
+        'Development Status :: 1 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Office/Business :: Financial :: Investment',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+    zip_safe=False
+)
--- tdameritradeapi/__init__.py
+++ tdameritradeapi/__init__.py
@@ -1,5 +1,4 @@
 import requests
-import os
 
 class TDAmeritradeAPI:
 	def __init__(self, api_key):
